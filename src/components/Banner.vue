02883c5ab01caac25c55e99418d27526696a862b
Lucas Machado <lukaasmachado143@gmail.com>
2023-05-02 22:03:08 -0300

    Banner criado na página Home

---
+++ src/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names
+        name: 'Banner'
+    }
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 420px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1{
+        background-color: #fcba0332;
+        color: #FFF;
+        text-align: center;
+        font-size: 60px;
+        border-radius: 50px;
+        width: 580px;
+        height: 80px;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="main-container">
+    <Banner />
     <h1>Home</h1>
   </div>
 </template>
-
-
 <script>
+  import Banner from '../components/Banner.vue';
+  export default{
+    name: 'HomeView',
+    components:{
+      Banner
+    }
+  }
 </script>
 
