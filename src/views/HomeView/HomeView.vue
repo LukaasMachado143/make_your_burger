22b15d3dfbc2f92df7ea41da0808cb8094b4621d
Lucas Machado <lukaasmachado143@gmail.com>
2023-05-03 07:57:50 -0300

    Organizando views e criando formulário para montar o hambuger.

---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-container">
+    <Banner />
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
+  </div>
+</template>
+<script>
+  import Banner from './components/Banner.vue';
+  import BurgerForm from './components/BurgerForm.vue';
+  export default{
+    name: 'HomeView',
+    components:{
+      Banner,
+      BurgerForm
+    }
+  }
+</script>
+<style scoped>
+  h1{
+    margin-top: 20px;
+  }
+</style>
+
---
+++ src/views/HomeView/components/BurgerForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>
+      <label for="clienteName">Nome do Cliente:</label>
+      <input type="text" name="clienteName" id="clienteName" placeholder="Digite seu nome...">
+    </div>
+
+    <div>
+      <label for="breadType">Tipo do Pão:</label>
+      <select name="breadType" id="breadType">
+        <option value="">Selecione o tipo do pão...</option>
+        <option value="frances">Pão Francês</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="meatType">Tipo da Carne:</label>
+      <select name="meatType" id="meatType">
+        <option value="">Selecione o tipo da carne...</option>
+        <option value="maninha">Maminha</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="optionalItems">Escolha seus opcionais:</label>
+      <div>
+        <input type="checkbox" name="salame" id="salame">
+        <label for="salame">Salame</label>  
+      </div>
+    </div>
+
+    <div>
+      <input type="button" value="Fazer Pedido">
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'BurgerForm'
+    }
+</script>
+
+<style>
+
+</style>
