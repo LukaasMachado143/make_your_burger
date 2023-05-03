5eda07ba327e66b6ed8d159b528cbf0e7d0dec3f
Lucas Machado <lukaasmachado143@gmail.com>
2023-05-03 12:55:04 -0300

    Efeito vidro inserido no formu;

--- src/views/HomeView/components/BurgerForm.vue
+++ src/views/HomeView/components/BurgerForm.vue
@@ -103,9 +103,17 @@ import MessageComponent from '../../../components/MessageComponent.vue';
 </script>
 
 <style scoped>
+
   #burger-form{
     max-width: 400px;
     margin: 0 auto;
+    background-color: rgba( 255, 255, 255, 0.35 );
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 13.5px );
+    -webkit-backdrop-filter: blur( 13.5px );
+    border-radius: 10px;
+    border: 1px solid rgba( 255, 255, 255, 0.18 );
+    padding: 15px;
   }
 
   .input-container{
